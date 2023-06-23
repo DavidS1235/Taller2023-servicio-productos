85291588a9277a89dc918be095ef945e4f33ddc8
DavidS1235 <davidsilvacordova@gmail.com>
2023-06-22 21:47:37 -0500

    added Eureka-client dependency

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableAutoConfiguration
 @ComponentScan
 public class DemoApplication {
